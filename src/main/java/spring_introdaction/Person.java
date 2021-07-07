2060aa7970330cedba45198b54cf14228f558293
Granickiy <puto45mexu>
2021-07-08 00:48:52 +0300

    Inversion of Control : XML Dependency Injection: Setter

--- src/main/java/spring_introdaction/Person.java
+++ src/main/java/spring_introdaction/Person.java
@@ -8,6 +8,16 @@ public class Person {
         this.pet = pet;
     }
 
+    public Person() {
+        System.out.println("Bean Person is created");
+    }
+
+    // setPet -> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Class person: set pet");
+        this.pet = pet;
+    }
+
     public void callYourPet(){
         System.out.println("Hello my lovely pet !");
         pet.say();
