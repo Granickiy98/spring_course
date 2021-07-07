8d119f64a6f658ebb158d24dd19fd2edf8a2ab43
Granickiy <puto45mexu>
2021-07-08 01:10:40 +0300

    Bean scope : singleton , prototype

--- src/main/java/spring_introdaction/Dog.java
+++ src/main/java/spring_introdaction/Dog.java
@@ -2,8 +2,18 @@ package spring_introdaction;
 
 public class Dog implements Pet{
 
+    private  String name;
+
     public Dog() {
         System.out.println("Bean Dog is Created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
