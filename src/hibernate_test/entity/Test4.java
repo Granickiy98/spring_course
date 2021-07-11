17212698298fe7ecbd554c78a5b387bddfe65677
Granickiy <puto45mexu>
2021-07-11 13:50:33 +0300

    hibernate update object

---
+++ src/hibernate_test/entity/Test4.java
@@ -0,0 +1,34 @@
+package hibernate_test.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.openSession();
+            session.beginTransaction();
+            session.createQuery("update Employee set salary = 1000 where name = 'Alexandr'").executeUpdate();
+//
+//            Employee employee = session.get(Employee.class,4);
+//
+//            employee.setName("Ivan");
+//            employee.setDepartment("HR");
+//            employee.setSurname("Ivanov");
+            session.getTransaction().commit();
+
+            System.out.println("DONE!");
+        } finally {
+            factory.close();
+        }
+
+    }
+}
